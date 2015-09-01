558126c04a1d52898ae99d45b4550001e5768d3c
Sacki2013 <Sacki2013@gmail.com>
2015-09-01 15:44:42 +0100

    Completed greedy.c

--- greedy/greedy.c
+++ greedy/greedy.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
-
+int main() {
   // Initialize Variables
-  int coinsTotal;
-  float changeDue, cents;
-  float quarter = 25;
-  float dime = 10;
-  float nickel = 5;
-  float pennies = 1;
+  int coinsTotal = 0,
+      quarter = 25,
+      dime = 10,
+      nickel = 5,
+      penny = 1,
+      cents;
+  float changeDue;
 
-  // Prompt the user to input amoumt.
+  // Prompt User to Input Change amount
   do {
-    printf("How much change are you owed? ($)(Format = 0.00): ");
-    scanf("%f", &changeDue);
-    // Convert to Cents
+    printf("How much change are you owed? (Format = 0.00)($): ");
+    scanf("%f", &changeDue );
+    // Convert to cents
     cents = changeDue * 100;
   } while(cents <= 0);
 
-  // Quarters
+  // Line 24 = quarters, Line 29 = dimes, Line 35 nickels, Line 41 pennies
+    // Uses a while loops to check if the number of cents remaining is greater
+    // than or equal to the coin in question, if it is it reduces by that coins
+    // value and adds 1 coin to cointTotal, this will loop until it fails and
+    // pass to an if statement checking if the cents left is zero, if it is it
+    // will output total coins, if not it will move to next denomination until
+    // complettion.
   while (cents >= quarter) {
     cents = cents - quarter;
     coinsTotal = coinsTotal + 1;
   } if (cents == 0) {
-    printf("The minimum number of coins is: %d\n", coinsTotal);
+      printf("The miminum number of coins is: %d\n", coinsTotal);
   } else {
-
-  // Dimes
-    while (cents >= dime) {
-      cents = cents - dime;
-      coinsTotal = coinsTotal + 1;
-    } if (cents == 0) {
-      printf("The minimum number of coins is: %d\n", coinsTotal);
-  } else {
-
-  // Nickels
-    while (cents >= nickel) {
-      cents = cents - nickel;
-      coinsTotal = coinsTotal + 1;
-    } if (cents == 0) {
-      printf("The minimum number of coins is: %d\n", coinsTotal);
+      while (cents >= dime) {
+        cents - dime;
+        coinsTotal = coinsTotal + 1;
+      } if (cents == 0) {
+          printf("The minimum number of coins is: %d\n", coinsTotal);
+      } else {
+          while (cents >= nickel) {
+            cents = cents - nickel;
+            coinsTotal = coinsTotal + 1;
+          } if (cents == 0) {
+              printf("The minimum number of coins is: %d\n", coinsTotal);
+          } else {
+              while (cents >= penny) {
+                cents = cents - penny;
+                coinsTotal = coinsTotal + 1;
+              } if (cents == 0) {
+                  printf("The minimum number of coins is: %d\n", coinsTotal);
+                }
+        }
+      }
     }
-  }
-}}
+}
